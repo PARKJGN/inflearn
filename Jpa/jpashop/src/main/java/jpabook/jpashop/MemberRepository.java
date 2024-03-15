bfa41c328ede44fcda96d3b5dfb909bf48b2a990
박종건 <patrol4@naver.com>
2024-03-15 19:34:59 +0900

    2024-03-15 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 - Database design - Entity Mapping     - Embeddable, Embedded     - * TO *

---
+++ Jpa/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+
+
+}
--- Jpa/jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ Jpa/jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,13 +1,22 @@
 package jpabook.jpashop;
 
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class JpashopApplicationTests {
+public class JpashopApplicationTests {
+
+	@Autowired
+	MemberRepository memberRepository;
 
 	@Test
-	void contextLoads() {
+	public void testMember() throws Exception {
+
 	}
 
 }
